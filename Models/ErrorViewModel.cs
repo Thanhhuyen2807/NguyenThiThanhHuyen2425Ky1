564e4eb99d09ae1537f7b568f8aa3674c5a83a8e
ThanhHuyen <thanhuyenxt28@gmail.com>
2024-10-18 00:09:17 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NguyenThiThanhHuyen080.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
